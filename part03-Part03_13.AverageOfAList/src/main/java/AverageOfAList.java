d8ecbd663e501bf62ba9030afc6e984ba8bea840
Luiz Lopes <luizclopes.soares@gmail.com>
2024-01-21 03:15:30 -0300

    Average of a list

---
+++ part03-Part03_13.AverageOfAList/src/main/java/AverageOfAList.java
@@ -0,0 +1,33 @@
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class AverageOfAList {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        ArrayList<Integer> numbers = new ArrayList<>();
+        
+        while (true) {
+            int input = Integer.valueOf(scanner.nextLine());
+            if (input == -1) {
+                break;
+            }
+
+            numbers.add(input);
+        }
+        
+        System.out.println("");
+
+        int sum = 0;
+
+        for (int number : numbers) {
+            sum += number;
+        }
+        
+        double average = (double) sum / numbers.size();
+        
+        System.out.println("Average: " + average);
+    }
+}
