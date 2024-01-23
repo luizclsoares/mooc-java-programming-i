42bb3bd7a5338343c18ac64cd8a17cf56bef2892
Luiz Lopes <luizclopes.soares@gmail.com>
2024-01-23 01:18:41 -0300

    AV Club

---
+++ part03-Part03_27.AVClub/src/main/java/AVClub.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+
+public class AVClub {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("")) {
+                break;
+            }
+
+            String[] strings = input.split(" ");
+
+            for (String string : strings) {
+                if(string.contains("av")){
+                    System.out.println(string);
+                }
+            }
+        }
+    }
+}
