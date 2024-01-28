2b34de40e8c3d4a053fdd23b3ed3de3f7efd22a6
Luiz Lopes <luizclopes.soares@gmail.com>
2024-01-28 09:13:42 -0300

    Numbers From a File

---
+++ part04-Part04_28.NumbersFromAFile/src/main/java/NumbersFromAFile.java
@@ -0,0 +1,34 @@
+
+import java.nio.file.Paths;
+import java.util.Scanner;
+
+public class NumbersFromAFile {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int count = 0;
+        
+        System.out.print("File? ");
+        String file = scanner.nextLine();
+        System.out.print("Lower bound? ");
+        int lowerBound = Integer.valueOf(scanner.nextLine());
+        System.out.print("Upper bound? ");
+        int upperBound = Integer.valueOf(scanner.nextLine());
+        
+        try(Scanner sc = new Scanner(Paths.get(file))){
+            while(sc.hasNextLine()){
+                int number = Integer.valueOf(sc.nextLine());
+                
+                if(number >= lowerBound && number <= upperBound){
+                    count++;
+                }
+            }
+        }catch(Exception e){
+            System.out.println("Error: " + e.getMessage());
+        }
+        
+        
+        System.out.println("Numbers: " + count);
+    }
+
+}
